856ca2a156a1feaca4f4ce8871097a018fed5dbf
Edan-Gordon <edangordon@gmail.com>
2025-03-10 19:51:54 +0100

    Fix build issues: update PostCSS config and add missing dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 /** @type {import('postcss').Config} */
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
     'tailwindcss': {},
     'autoprefixer': {},
-  }
+  },
 } 
